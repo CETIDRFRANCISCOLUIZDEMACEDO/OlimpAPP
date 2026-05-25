9417c3f2db0c5af8a8b0d99b616d011e2a7f343f
Luiz Henrique <luiz.silva.25@aluno.edu.pi.gov.br>
2026-05-25 09:16:24 -0300

    Update AulaVideoScreen.jsx

--- frontend/src/screens/AulaVideoScreen.jsx
+++ frontend/src/screens/AulaVideoScreen.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { getWatchedAulas, markAsWatched } from '../services/aulasService'
 
 const CheckIcon = () => (
